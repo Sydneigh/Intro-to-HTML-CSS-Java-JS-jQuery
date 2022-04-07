e3760976efe0636a19be2dfe4a9f63d809a9bb02
Sydneigh Vitlen <96391154+Sydneigh@users.noreply.github.com>
2022-04-07 15:04:51 +0300

    Add files via upload

---
+++ Challenge 13.js
@@ -0,0 +1,13 @@
+//Challenge: My Favorite Foods
+//First, a heading!
+
+//We're making a list of your favorite foods, but first we need a header. Write 'My Favorite Foods' near the top of the canvas.
+fill(4, 0, 255);
+textSize(25);
+text("My Favorite Foods", 100, 60);
+text("1.Pasta",100, 90);
+text("2.cheese", 100, 110);
+text("3.broccoli", 100, 140);
+
+
+
