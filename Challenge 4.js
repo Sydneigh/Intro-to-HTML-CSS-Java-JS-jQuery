2db02ec060d48d3c868b264cdef0a8cc0024a77a
Sydneigh Vitlen <96391154+Sydneigh@users.noreply.github.com>
2022-04-07 14:38:04 +0300

    Add files via upload

---
+++ Challenge 4.js
@@ -0,0 +1,31 @@
+//Project: What's for dinner?
+//The program below only includes a simple plate, it's up to you to put dinner on the plate. Use shapes like rect(), ellipse(), and triangle() to draw food on the plate. Use fill() and stroke() to color them.
+background(186, 145, 20); // wooden table
+ellipse(200, 200, 350, 350); // plate
+ellipse(200, 200, 300, 300); 
+
+fill (106, 0, 255);
+bezier (100, 130, 500, 340, 350, -40, 120, 90);
+
+fill (79, 20, 29);
+ellipse(122, 124, 100, 60);//aubergine
+
+fill (17, 122, 61);
+ellipse(200, 200, 70, 70); //brussle sprout
+fill (17, 122, 61);
+ellipse(265, 201, 70, 70); //brussle sprout
+
+fill (17, 122, 61);
+ellipse(230, 245, 70, 70); //brussle sprout
+
+fill (17, 122, 61);
+ellipse(277, 245, 70, 70); //brussle sprout
+
+fill (242, 255, 0);
+rect (230, 130, 50, 50); // butter
+stroke (1, 84, 0);
+strokeWeight (20);
+line(250, 169, 83, 170);// asparagus
+
+
+
