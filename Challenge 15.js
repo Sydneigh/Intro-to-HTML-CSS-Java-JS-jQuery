027b493a86ec49ea692353f90b0e5f847497984a
Sydneigh Vitlen <96391154+Sydneigh@users.noreply.github.com>
2022-04-07 15:05:19 +0300

    Add files via upload

---
+++ Challenge 15.js
@@ -0,0 +1,21 @@
+//Project: Ad design
+//In the text challenges, you showed that you know how to use text commands. Now, use that knowledge to make an advertisement for a real or made-up product!
+
+    //Change the big headline to the name of your product.
+    //Add at least 2 more text function calls advertising the product.
+    //Change the text size so that you display text in at least 3 sizes.
+    //Add a relevant drawing with at least 3 shape or image function calls.
+    //Bonus: Play around with color and positioning to make it more flashy. If you want to go really wild, add an animation!
+
+    fill(84, 56, 194);
+textSize(60);
+text("Sony", 60, 60);
+textSize(50);
+text("HD TV", 60, 250);
+textSize(45);
+text("Touch screen", 10, 210);
+fill(26, 18, 18);
+ellipse(136, 136, 64, 40);
+rect(151, 104, 89, 60);
+ellipse(70, 111, 89, 89);
+
