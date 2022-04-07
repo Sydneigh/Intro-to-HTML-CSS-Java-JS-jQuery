44ac5a2b3513ea40ce51c27a5acbee0eef7351d5
Sydneigh Vitlen <96391154+Sydneigh@users.noreply.github.com>
2022-04-07 14:33:00 +0300

    Add files via upload

---
+++ Challenge 2.js
@@ -0,0 +1,8 @@
+//Challenge: Waving snowman
+//In this first step, I'll use the rect() command to draw the ground, so that the snowman isn't just floating in air. I'll  add  rect() command before the ellipse() commands for the snowman, so that the snowman looks like he is in front of the ground.
+rect (4, 347, 400, 50);
+ellipse(200, 300, 150, 150);
+ellipse(200, 200, 100, 100);
+ellipse(200, 120, 75, 75);
+line(160, 200, 50, 150);
+line(240, 200, 50, 250);
