24552574614ed65688ca6cc1b9b6fa2671446351
Sydneigh Vitlen <96391154+Sydneigh@users.noreply.github.com>
2022-04-07 14:15:03 +0300

    Add files via upload
    Challenge: Simple snowman | Exercises for the Khan Academy.

---
+++ Challenge 1.js
@@ -0,0 +1,11 @@
+//Exercises for the Khan Academy
+//Challenge: Simple snowman
+//In this challenge, I'll draw a snowman using the ellipse() command.
+
+//Start off by making a circle, for the bottom of the snowman, with an ellipse() command. 
+//  bottom circle:
+ellipse (168, 319, 155, 150);
+// middle circle:
+ellipse (168, 210, 100, 100);
+// top circle:
+ellipse (171, 137, 70, 70);
